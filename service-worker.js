1d842dbb4053bc70172db4e2dd0ac9851f10582a
shi674687-sudo <shi674687@gmail.com>
2026-04-30 19:50:04 +0900

    Add push notification support in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,9 @@
-
+self.addEventListener("push", event => {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icon-192.png"
+    })
+  );
+});
